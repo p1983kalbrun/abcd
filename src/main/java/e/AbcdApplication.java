2089a0b4db32c0d4becf5bd491200d7f095f478b
p1983kalbrun <53264036+p1983kalbrun@users.noreply.github.com>
2019-08-21 13:22:42 +0100

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/e/AbcdApplication.java
@@ -0,0 +1,12 @@
+package e;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcdApplication {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(AbcdApplication.class, args);
+	}
+
+}
